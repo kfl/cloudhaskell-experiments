1b7af75e9a818be26335be874fbcdb9c41776968
Brian Avlund <brian.avlund@gmail.com>
2012-03-16 00:43:01 +0100

    Made a function for making the desired number of processes, and shortened the timeout.

--- rtlb.hs
+++ rtlb.hs
@@ -14,7 +14,7 @@ import Remote
 -- NOTE: it seems that receiveTimeout is faulty and do not reset the timer in a new iterationc.
 -- next: id of the next process in the loop
 receive_send :: ProcessId -> ProcessM ()
-receive_send next = do {res <- receiveTimeout 300000 [match (\x -> return (x::Int))];
+receive_send next = do {res <- receiveTimeout 200000 [match (\x -> return (x::Int))];
                         case res of
      					   	 Nothing  -> do {selfP <- getSelfPid;
 							     			 say ((show selfP) ++ " DIED");
@@ -26,18 +26,17 @@ $( remotable ['receive_send] )
 
 initialProcess "MASTER" = do {self <- getSelfNode;
                               selfP <- getSelfPid;
-							  p1 <- spawnLocal (receive_send selfP);
-							  p2 <- spawnLocal (receive_send p1);
-							  p3 <- spawnLocal (receive_send p2);
-							  p4 <- spawnLocal (receive_send p3);
-							  p5 <- spawnLocal (receive_send p4);
-							  n <- receiveLoop 50 0 p5;
+							  pn <- spawnN 10 selfP;
+							  n <- receiveLoop 1000 0 pn;
 							  say ("Finished with n = " ++ (show n))}
 						  where
+							  spawnN 0 p = return p
+							  spawnN n p0 = do {p <- spawnLocal (receive_send p0);
+							                    spawnN (n-1) p}
 							  receiveLoop 0 n _ = return n
 							  receiveLoop i n pid = do {send pid ((n+1)::Int);
 							   					        res <- receiveWait [match (\x -> return (x::Int))];
-													    say (("ITERATION ") ++ (show i));
+													    --say (("ITERATION ") ++ (show i));
 														receiveLoop (i-1) (res) pid}
 
 initialProcess _ = say "You need to start this program as either a MASTER or a WORKER. Set the appropiate value of cfgRole on the command line or in the config file."
