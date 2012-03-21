c1e4dbccb63c7fb36562d31ae11568694bbbdd0f
Brian Avlund <brian.avlund@gmail.com>
2012-03-21 22:28:59 +0100

    Have tried to send FList through the network but without success

--- rtlb.hs
+++ rtlb.hs
@@ -6,85 +6,90 @@
   Brian Avlund
 -}
 
-{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE DeriveDataTypeable,TemplateHaskell #-}
 module RTLB where
 	
 import Remote
+import Remote.Call (mkClosure)
+import Data.Binary (Binary,get,put,encode,decode)
 
 {- ------------------ Flist ------------------ -}
-type FList a = Maybe Int -> [a]
+type FList = Maybe Int -> [String]
 
-empty :: FList a
+instance Binary FList where 
+                put = genericPut
+                get = genericGet
+
+
+empty :: FList
 empty = \ _ -> []
 
-add :: a -> FList a -> FList a
-add x fl = \ pre -> 
+add :: String -> FList -> FList
+add x fl = \ pre ->
   case pre of
     Nothing        -> x : fl Nothing
     Just n | n > 0 -> x : (fl $ Just $ n-1)
     _              -> []
         
-fromList :: [a] -> FList a
+fromList :: [String] -> FList
 fromList xs = foldr add empty xs
 
-toList :: FList a -> [a]
+toList :: FList -> [String]
 toList fl = fl Nothing
-        
-ftake :: Int -> FList a -> [a]
-ftake n fl = fl $ Just n
 
+ftake :: Int -> FList -> [String]
+ftake n fl = fl $ Just n
 
 {- ------------------ Round trip latency benchmark ------------------ -}
 -- NOTE: it seems that receiveTimeout is faulty and do not reset the timer in a new iterationc.
 -- next: id of the next process in the loop
-receive_send :: ProcessId -> ProcessM ()
-receive_send next = do {res <- receiveTimeout 200000 [match (\x -> return (x::Int))];
+-- tag: temperary identifier
+receive_send :: ProcessId -> String -> ProcessM ()
+receive_send next tag = do {res <- receiveTimeout 200000 [match (\x -> return (x::Closure String))];
                         case res of
      					   	 Nothing  -> do {selfP <- getSelfPid;
 							     			 say ((show selfP) ++ " DIED");
 							                 return ()}
-     					   	 Just ans -> do {send next (ans + 1);
-					                         receive_send next}}
-					   
+     					   	 Just ans -> do {flist <- invokeClosure ans;
+							 				 case flist of
+												 Nothing -> return ()
+												 Just fl -> do {send next ($(mkClosure 'add) tag fl);
+					                         				    receive_send next tag}}}				  
 
--- spawnN 0 p = return p
--- spawnN n p0 = do {p <- spawnLocal (receive_send p0);
---                   	   spawnN (n-1) p}
--- 
--- spawnW nid 0 p = return p
--- spawnW nid n p0 = do {p <- spawn nid (receive_send p0);
---                   	  spawnN nid (n-1) p}
-					  
 
-$( remotable ['receive_send] )
+$( remotable ['receive_send, 'add] )
+		
 		
 spawnN :: [NodeId] -> Int -> ProcessId -> ProcessM ProcessId			  
 spawnN nodes 0 p = return p
-spawnN nodes n p0 = do {p1 <- spawnLocal (receive_send p0);
+spawnN nodes n p0 = do {p1 <- spawnLocal (receive_send p0 "this");
                         pn <- spawnOnNodes nodes p1;
 						spawnN nodes (n-1) pn}
+			
 				
 spawnOnNodes :: [NodeId] -> ProcessId -> ProcessM ProcessId
 spawnOnNodes [] p = return p
-spawnOnNodes (n:nodes) p0 = do {p1 <- spawn n (receive_send__closure p0);
+spawnOnNodes (n:nodes) p0 = do {p1 <- spawn n (receive_send__closure p0 "other");
                                 spawnOnNodes nodes p1}
-
-
 
 
 initialProcess "MASTER" = do {selfP <- getSelfPid;
 							  peers <- getPeers;
-							  let {workers = findPeerByRole peers "WORKER"};
-							  pn <- spawnN workers 10 selfP;
-							  n <- receiveLoop 1000 0 pn;
+							  let {workers = findPeerByRole peers "WORKER";
+							       flist = empty};
+							  pn <- spawnN workers 5 selfP;
+							  n <- receiveLoop 1 flist pn;
 							  say ("Finished with n = " ++ (show n))}
 						  where
-							  receiveLoop 0 n _ = return n
-							  receiveLoop i n pid = do {send pid ((n+1)::Int);
-							   					        res <- receiveWait [match (\x -> return (x::Int))];
-													    --say (("ITERATION ") ++ (show i));
-														receiveLoop (i-1) (res) pid}
+							  receiveLoop 0 f _ = return f
+							  receiveLoop i f pid = do {send pid ('add "master" f);
+							   					        res <- receiveWait [match (\x -> return (x::Closure String))];
+														do {flst <- invokeClosure res;
+															case flst of
+															    Nothing -> return ()
+															    Just fl -> do {receiveLoop (i-1) fl pid}}}
 
+-- initialProcess "WORKER" = receiveTimeout 200000  []							
 initialProcess "WORKER" = receiveWait []							
 
 initialProcess _ = say " MASTER or WORKER."
