d08a276ab77efe2b53f858fd4364b30689a0f019
Brian Avlund <brian.avlund@gmail.com>
2012-03-23 15:12:07 +0100

    Erlang code is now able to oscillate between known nodes

--- rtlb.erl
+++ rtlb.erl
@@ -13,7 +13,7 @@ empty() -> fun(_) -> [] end.
 
 add(X, Fl) -> fun(Pre) ->
 			  		case Pre of
-			  			{nothing} -> [X | Fl({nothing})];
+			  			nothing -> [X | Fl(nothing)];
 			  			{just, N} when N > 0 -> [X | Fl({just, N-1})];
 						_ -> []
 			  		end
@@ -21,29 +21,36 @@ add(X, Fl) -> fun(Pre) ->
  
 fromList(Xs) -> lists:foldr(fun add/2, empty(), Xs).
 			  
-toList(Fl) -> Fl({nothing}).
+toList(Fl) -> Fl(nothing).
 
 fTake(N, Fl) -> Fl({just, N}).
 
 
 %% ------------------ Round trip latency benchmark ------------------ %%
-receive_send(Next) -> receive
-					      {N} -> Next ! {N+1},
-                                 receive_send(Next)
-					  after 
-					      2000 -> io:fwrite("~p DIED ~n", [self()])
-    				  end.
+receive_send(Next, Tag) -> receive
+					       		N -> Next ! add(Tag, N),
+                           		receive_send(Next, Tag)
+					       after 
+					       		2000 -> io:fwrite("~p DIED ~n", [self()])
+    				       end.
 
-main(master) -> P = spawnN(10, self()),
-                {Time, Res} = timer:tc(fun rtlb:receiveLoop/3, [1000, 0, P]),
-                io:fwrite("Result: ~p in ~p microseconds",[Time, Res]).
-
-spawnN(0, P) -> P;
-spawnN(N, P0) -> spawnN(N-1, spawn(fun() -> receive_send(P0) end)).
+spawnN(_, 0, P) -> P;
+spawnN(Nodes, N, P0) -> P1 = spawn(fun() -> receive_send(P0, "this") end),
+						spawnN(Nodes, N-1, spawnOnNodes(Nodes, P1)).
+						
+spawnOnNodes([], Pid) -> Pid;
+spawnOnNodes([N|Nodes], P0) -> P1 = spawn(N, fun() -> receive_send(P0, "other") end),
+							   		spawnOnNodes(Nodes, P1).
+					  
+main(master) -> Nodes = [node1@Avlund], % localhost is apparently working on mac's
+io:format("~p", [Nodes]),	
+                P = spawnN(Nodes, 10, self()),
+                {Time, Res} = timer:tc(fun rtlb:receiveLoop/3, [1, empty(), P]),
+                io:fwrite("Result: ~p in ~p microseconds",[Time, toList(Res)]).
 
 receiveLoop(0, N, _) -> N;
-receiveLoop(I, N, Pid) -> Pid ! {N+1},
+receiveLoop(I, N, Pid) -> Pid ! N,
                           receive
-                              {M} -> receiveLoop(I-1, M, Pid)
+                              M -> receiveLoop(I-1, M, Pid)
                           end.
 
