e1d3420fe30217a300cfdb98dada1a41f6a643ad
Brian Avlund <brian.avlund@gmail.com>
2012-03-26 19:12:03 +0200

    The tree structure is now working for haskell as well. Some debugging outputs are still present.
    Had some problems where the worker node kept registering itself as
    master although the worker claus of initialProcess* was triggered.

--- rtlb.hs
+++ rtlb.hs
@@ -37,69 +37,137 @@ fromList xs = foldr add empty xs
 toList :: FList -> [String]
 toList fl = fl Nothing
 
+merge :: FList -> FList -> FList
+merge xs ys = foldr add ys $ toList xs
+
 ftake :: Int -> FList -> [String]
 ftake n fl = fl $ Just n
 
 {- ------------------ Simple list ------------------ -}
 type SList = [String]
 
+emptyS :: SList
+emptyS = []
+
 addS :: String -> SList -> SList
 addS x l = x : l
 
-emptyS :: SList
-emptyS = []
+fromListS :: [String] -> SList
+fromListS xs = xs -- foldr addS empty xs
 
 toListS :: [String] -> [String]
 toListS l = l
 
+mergeS :: SList -> SList -> SList
+mergeS xs ys = foldr addS ys $ toListS xs
+
+stake :: Int -> SList -> [String]
+stake n fl = take n fl
+
 
 {- ------------------ Round trip latency benchmark ------------------ -}
--- NOTE: it seems that receiveTimeout is faulty and do not reset the timer in a new iterationc.
--- next: id of the next process in the loop
--- tag: temperary identifier
-receive_send :: ProcessId -> String -> ProcessM ()
-receive_send next tag = do {res <- receiveTimeout 200000 [match (\x -> return (x::SList))];
+-- NOTE: it seems that receiveTimeout is faulty and do not reset the timer in a new iteration.
+-- tag: temporary identifier
+receive_sendR :: ProcessId -> String -> ProcessM ()
+receive_sendR next tag = do {res <- receiveTimeout 200000 [match (\x -> return (x::SList))];
                         	case res of
      							Nothing  -> do {selfP <- getSelfPid;
 												say ((show selfP) ++ " DIED");
 												return ()}
      							Just fl -> do {	send next (addS tag fl);
-					    						receive_send next tag}}				  
+					    						receive_sendR next tag}}				  
 
+receive_sendT :: (ProcessId, ProcessId) -> String -> ProcessM ()
+receive_sendT (lc, rc) tag = do {res <- receiveTimeout 200000 [match (\x -> return (x::SList))];
+								case res of
+     						  	 	Nothing  -> do { selfP <- getSelfPid;
+								 				 	 say ((show selfP) ++ " DIED");
+												 	 return ()}
+     							 	Just fl -> do { let {nlist = addS tag fl};
+     							 				 	send lc nlist;
+	     							 				send rc nlist;
+						    						receive_sendT (lc, rc) tag}}
 
-$( remotable ['receive_send] )
+$( remotable ['receive_sendR, 'receive_sendT] )
 		
 		
-spawnN :: [NodeId] -> Int -> ProcessId -> ProcessM ProcessId			  
-spawnN nodes 0 p = return p
-spawnN nodes n p0 = do {p1 <- spawnLocal (receive_send p0 "this");
+spawnR :: [NodeId] -> Int -> ProcessId -> ProcessM ProcessId			  
+spawnR nodes 0 p = return p
+spawnR nodes n p0 = do {p1 <- spawnLocal (receive_sendR p0 "this");
                         pn <- spawnOnNodes nodes p1;
-						spawnN nodes (n-1) pn}
+						spawnR nodes (n-1) pn}
 			
 				
 spawnOnNodes :: [NodeId] -> ProcessId -> ProcessM ProcessId
 spawnOnNodes [] p = return p
-spawnOnNodes (n:nodes) p0 = do {p1 <- spawn n (receive_send__closure p0 "other");
+spawnOnNodes (n:nodes) p0 = do {p1 <- spawn n (receive_sendR__closure p0 "other");
                                 spawnOnNodes nodes p1}
 
 
 initialProcess "MASTER" = do {selfP <- getSelfPid;
 							  peers <- getPeers;
+							  say ("workers = " ++ (show $ findPeerByRole peers "MASTER"));
 							  let {workers = findPeerByRole peers "WORKER";
 							       flist = emptyS};
-							  pn <- spawnN workers 5 selfP;
+							  pn <- spawnR workers 5 selfP;
 							  fl <- receiveLoop 1 flist pn;
 							  say ("Finished with n = " ++ (show $ toListS fl))}
 							  -- say ("Finished with n = " ++ (show $ fl))}
 						  where
 							  receiveLoop 0 f _ = return f
-							  receiveLoop i f pid = do {send pid (addS "master" f);
+							  receiveLoop i f pid = do {send pid f;
 							   					        fl <- receiveWait [match (\x -> return (x::SList))];
 							   					        receiveLoop (i-1) fl pid}
 
 -- initialProcess "WORKER" = receiveTimeout 200000  []							
-initialProcess "WORKER" = receiveWait []							
+initialProcess "WORKER" = do { selfP <- getSelfPid;
+								say ("worker node " ++ (show selfP));
+							   peers <- getPeers;
+							   say ("workers = " ++ (show $ findPeerByRole peers "WORKER"));
+							   say ("peers = " ++ (show $ peers));
+							   receiveWait []}
 
-initialProcess _ = say " MASTER or WORKER."
+initialProcess _ = say "MASTER or WORKER."
 
 main = remoteInit (Just "config") [RTLB.__remoteCallMetaData] initialProcess 
+
+
+{- ------------------ Tree structure ------------------ -}
+				  
+
+spawnT :: Integer -> NodeId -> ProcessId -> ProcessM (ProcessId, ProcessId)
+spawnT 0 nodes self = return (self, self)
+spawnT s node self = do {c0 <- spawnT (s-1) node self;
+				   	  	 lc <- spawnLocal (receive_sendT c0 "this");
+				   	  	 c1 <- spawnT (s-1) node self;
+					  	 rc <- spawn node (receive_sendT__closure c1 "other");
+				   	  	 return (lc, rc)}
+
+gather_list :: Integer -> SList -> ProcessM SList
+gather_list 0 list = return list
+gather_list i list = do {fl <- receiveWait [match (\x -> return (x::SList))];
+					  	 gather_list (i-1) $ mergeS fl list}
+
+initialProcessT "MASTER" = do { let {layers = 1};
+							   	selfP <- getSelfPid;
+							   	peers <- getPeers;
+							    let {w:ws = findPeerByRole peers "WORKER";
+							         flist = addS "master" emptyS};
+							    pn <- spawnT layers w selfP;
+								fl <- receiveLoop 1 flist pn layers;
+							   	say ("Finished with n = " ++ (show $ toListS fl))}
+						   where
+								receiveLoop 0 f _ _ = return f
+							   	receiveLoop i f (lc, rc) layers = do {send lc f;
+							   										  send rc f;
+							    					         	 	  fl <- gather_list (2^(layers+1)) f;
+							    					      		 	  receiveLoop (i-1) fl (lc, rc) layers}
+
+-- initialProcess "WORKER" = receiveTimeout 200000  []							
+initialProcessT "WORKER" = do { peers <- getPeers;
+							    say ("peers = " ++ (show $ peers));
+							    receiveWait []}						
+
+initialProcessT _ = say " MASTER or WORKER."
+
+mainT = remoteInit (Just "config") [RTLB.__remoteCallMetaData] initialProcessT 
